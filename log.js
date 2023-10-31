792df7323a17372aefa70690d30e77ca485c60c4
Hironobu Sano <h-sano@omni-s.co.jp>
2023-10-31 19:20:51 +0900

    ログ取得

---
+++ log.js
@@ -0,0 +1,48 @@
+const crypto = require('crypto');
+
+// シグネチャ
+const signature = 'omnidb'
+
+/**
+ * デバッグモードかどうか
+ */
+exports.isDebug = process.env.DEBUG_OMNIDB ? true : false;
+
+/**
+ * ログ用のIDを生成します。in/outなどでログを紐付けるために使用します。
+ * @returns {string} ログ用のID
+ */
+exports.genLogId = () => {
+  return crypto.randomBytes(4).toString('hex');
+}
+
+/**
+ * ログメッセージを整形します。
+ * @param {string} s ログメッセージ
+ * @returns {string} 整形されたログメッセージ
+ */
+exports.getLogMsg = (s) => {
+  if (!s) {
+    return '';
+  }
+
+  // ログだと改行があると見ずらいので、改行をスペースに変換する
+  // 行の先頭と末尾のスペースも削除
+  const lines = s.split(/\r?\n/);
+  const trimmedLines = lines.map(line => line.trim());
+  const result = trimmedLines.join(' ').trim();
+  return result;
+}
+
+/**
+ * デバッグログを出力します。
+ * @param  {...any} args デバッグログに出力する内容
+ */
+exports.debugLog = (...args) => {
+  if (this.isDebug) {
+    const now = new Date();
+    const time = now.toLocaleTimeString() + '.' + String(now.getMilliseconds()).padStart(3, '0');
+    console.log(`[${signature}] ${time} :`, ...args);
+  }
+}
+
--- omnidb.js
+++ omnidb.js
@@ -1,5 +1,10 @@
 const OmniDbNative = require('bindings')('omnidb');
 
+const log = require('./log.js');
+const debugLog = log.debugLog;
+const genLogId = log.genLogId;
+const getLogMsg = log.getLogMsg;
+
 const {
   isAS400, getAS400Schemas, getAS400CurrentSchema
 } = require('./as400.js');
@@ -26,26 +31,47 @@ class OmniDb {
     this._native = new OmniDbNative();
     this._dbms = '';
   }
+
+  static set debug(value) {
+    log.isDebug = value;
+  } 
+
+  static get debug() {
+    return log.isDebug;
+  }
+
   drivers() {
     return new Promise((resolve) => {
-      resolve(JSON.parse(this._native.drivers()));
+      const lid = genLogId();
+      debugLog('drivers', '<fid>', lid);
+
+      const resJson = this._native.drivers();
+
+      debugLog('drivers', '<res>', resJson, '<fid>', lid);
+      resolve(JSON.parse(resJson));
     });
   }
   connect(connectionString) {
     return new Promise((resolve) => {
+      const lid = genLogId();
+      debugLog('connect', '<cnn>', getLogMsg(connectionString), '<fid>', lid);
       const result = this._native.connect(connectionString)
       if(result) {
         this._dbms = this._native.dbms();
       }
+      debugLog('connect', '<res>', result, '<fid>', lid);
       resolve(result);
     });
   }
   disconnect() {
     return new Promise((resolve) => {
+      const lid = genLogId();
+      debugLog('disconnect', '<db>', this.dbms(), '<fid>', lid);
       const result = this._native.disconnect();
       if(result) {
         this._dbms = '';
       }
+      debugLog('disconnect', '<res>', result, '<fid>', lid);
       resolve(result);
     });
   }
@@ -54,14 +80,20 @@ class OmniDb {
   }
   schemas(condition = {}) {
     return new Promise((resolve) => {
+      const lid = genLogId();
+
+      debugLog('schemas', '<cond>', JSON.stringify(condition), '<db>', this.dbms(), '<fid>', lid);
+
       if(isAS400(this.dbms())) {
         // AS400はODBCのSQLTablesではうまく取得できないのでSQLで取得する
         getAS400Schemas(this).then((schemas) => {
+          debugLog('schemas', '<res>', JSON.stringify(schemas), '<fid>', lid);
           resolve(schemas);
         });
       } else if(isMySQL(this.dbms())) {
         // MySQLはスキーマ名がODBCから取得できないためSQLで取得する
         getMySQLSchemas(this).then((schemas) => {
+          debugLog('schemas', '<res>', JSON.stringify(schemas), '<fid>', lid);
           resolve(schemas);
         });
       } else {
@@ -78,6 +110,8 @@ class OmniDb {
             remarks: '',
           }
         })
+
+        debugLog('schemas', '<res>', JSON.stringify(schemas), '<fid>', lid);
         resolve(schemas);
       }
     });
@@ -85,83 +119,133 @@ class OmniDb {
   currentSchema() {
     // カレントスキーマの取得はODBC経由では行えないのでSQLで取得する
     return new Promise((resolve) => {
+      const lid = genLogId();
+
+      debugLog('currentSchema', '<db>', this.dbms(), '<fid>', lid);
+
       if(isAS400(this.dbms())) {
         getAS400CurrentSchema(this).then((schema) => {
+          debugLog('currentSchema', '<res>', schema, '<fid>', lid);
           resolve(schema);
         });
       } else if(isMySQL(this.dbms())) {
         getMySQLCurrentSchema(this).then((schema) => {
+          debugLog('currentSchema', '<res>', schema, '<fid>', lid);
           resolve(schema);
         });
       } else if(isPostgres(this.dbms())) {
         getPostgresCurrentSchema(this).then((schema) => {
+          debugLog('currentSchema', '<res>', schema, '<fid>', lid);
           resolve(schema);
         });
       } else {
         // その他の場合はカレントスキーマは空文字を返す
+        debugLog('currentSchema', '<res>', '', '<fid>', lid);
         resolve('');
       }
     });
   }
   tables(condition) {
     return new Promise((resolve) => {
-      let tables = JSON.parse(this._native.tables(condition));
+      const lid = genLogId();
+      debugLog('tables', '<cond>', JSON.stringify(condition), '<db>', this.dbms(), '<fid>', lid);
+
+      const resJson = this._native.tables(condition);
+
+      debugLog('tables', '<res #1>', resJson, '<fid>', lid);
+    
+      let tables = JSON.parse(resJson);
       if(isPostgres(this.dbms())) {
         // PostgreSQLはテーブル名がODBCから取得できないためSQLで取得する
         setPostgresTables(this, tables).then((t) => {
+          debugLog('tables', '<res #2>', JSON.stringify(t), '<fid>', lid);
           resolve(t);
         });
       } else {
         if(isMySQL(this.dbms())) {
           tables = getMySQLTables(tables);
         }
+        debugLog('tables', '<res #2>', JSON.stringify(tables), '<fid>', lid);
         resolve(tables);
       }
     });
   }
   columns(condition) {
     return new Promise((resolve) => {
-      let columns = JSON.parse(this._native.columns(condition));
+      const lid = genLogId();
+      debugLog('columns', '<cond>', JSON.stringify(condition), '<db>', this.dbms(), '<fid>', lid);
+    
+      const resJson = this._native.columns(condition);
+
+      debugLog('columns', '<res #1>', resJson, '<fid>', lid);
+
+      let columns = JSON.parse(resJson);
       if(isPostgres(this.dbms())) {
         // PostgreSQLはカラムのRemarkがcolumnsからは取得できないので、SQLで取得する
         setPostgresColumns(this, columns).then((c) => {
+          debugLog('columns', '<res #2>', JSON.stringify(c), '<fid>', lid);
           resolve(c);
         });
       } else {
         if(isMySQL(this.dbms())) {
           columns = getMySQLColumns(columns);
         }
+
+        debugLog('columns', '<res #2>', JSON.stringify(columns), '<fid>', lid);
         resolve(columns);
       }
     });
   }
   primaryKeys(condition = {}) {
     return new Promise((resolve) => {
+      const lid = genLogId();
+      debugLog('primaryKeys', '<cond>', JSON.stringify(condition), '<db>', this.dbms(), '<fid>', lid);
+
       // 主キー情報を取得する
       let keys = JSON.parse(this._native.primaryKeys(condition));
       if(isMySQL(this.dbms())) {
         keys = getMySQLPrimaryKeys(keys);
       }
+
+      debugLog('primaryKeys', '<res>', JSON.stringify(keys), '<fid>', lid);
       resolve(keys);
     });
   }
   query(queryString, options) {
     return new Promise((resolve) => {
+      const lid = genLogId();
+      debugLog('query', '<sql>', getLogMsg(queryString), '<opt>', JSON.stringify(options), '<db>', this.dbms(), '<fid>', lid);
+
       let result = JSON.parse(this._native.query(queryString, options));
       if(isMySQL(this.dbms())) {
         result = getMySQLQuery(result);
       }
+
+      debugLog('query', '<res>', JSON.stringify(result), '<fid>', lid);
       resolve(result);
     });
   }
   execute(sql) {
     return new Promise((resolve) => {
-      resolve(JSON.parse(this._native.execute(sql)));
+      const lid = genLogId();
+      debugLog('execute', '<sql>', getLogMsg(sql), '<db>', this.dbms(), '<fid>', lid);
+
+      const resJson = this._native.execute(sql);
+
+      debugLog('execute', '<res>', resJson, '<fid>', lid);
+
+      resolve(JSON.parse(resJson));
     });
   }
   records(sql) {
     return new Promise((resolve) => {
-      resolve(JSON.parse(this._native.records(sql)));
+      const lid = genLogId();
+      debugLog('records', '<sql>', getLogMsg(sql), '<db>', this.dbms(), '<fid>', lid);
+
+      const resJson = this._native.records(sql);
+      debugLog('records', '<res>', resJson, '<fid>', lid);
+
+      resolve(JSON.parse(resJson));
     });
   }
   setLocale(category, locale) {
